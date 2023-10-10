cf9079edeebfd57685997af1fce409177eb3741b
Stefanos Kyrikakis <kirikakis@gmail.com>
2023-10-10 23:43:18 +0100

    fix gimbal

--- Code/Server/GimbalControl.py
+++ Code/Server/GimbalControl.py
@@ -1,61 +1,44 @@
-import time
-from PCA9685 import PCA9685
 import smbus
-import RTIMU
+import time
 import math
+import RTIMU
 from servo import Servo
 
-class GimbalControl:
-    def __init__(self):
-        self.servo_pan = Servo()
-        self.servo_tilt = Servo()
+SETTINGS_FILE = "RTIMULib"
 
-        SETTINGS_FILE = "RTIMULib"
-        self.imu = RTIMU.RTIMU(RTIMU.Settings(SETTINGS_FILE))
+s = RTIMU.Settings(SETTINGS_FILE)
+imu = RTIMU.RTIMU(s)
 
-        if not self.imu.IMUInit():
-            raise Exception("IMU Init Failed")
+if not imu.IMUInit():
+    print("IMU Init Failed")
+else:
+    imu.setSlerpPower(0.02)
+    imu.setGyroEnable(True)
+    imu.setAccelEnable(True)
+    imu.setCompassEnable(False)
 
-        self.imu.setSlerpPower(0.02)
-        self.imu.setGyroEnable(True)
-        self.imu.setAccelEnable(True)
-        self.imu.setCompassEnable(False)
+    poll_interval = imu.IMUGetPollInterval()
+    print("Poll Interval: %d ms" % poll_interval)
 
-        self.poll_interval = self.imu.IMUGetPollInterval()
+    # Initialize the Servo class for controlling servos
+    servo = Servo()
 
-    def start(self):
-        try:
-            while True:
-                if self.imu.IMURead():
-                    data = self.imu.getIMUData()
-                    fusionPose = data["fusionPose"]
-                    roll = math.degrees(fusionPose[0]) - 90
-                    pitch = math.degrees(fusionPose[1])
-                    yaw = math.degrees(fusionPose[2])
+    try:
+        while True:
+            if imu.IMURead():
+                data = imu.getIMUData()
+                fusionPose = data["fusionPose"]
+                roll = math.degrees(fusionPose[0]) -90
+                pitch = math.degrees(fusionPose[1])
+                yaw = math.degrees(fusionPose[2])
+                print(f"Roll: {roll:.2f} degrees, Pitch: {pitch:.2f} degrees, Yaw: {yaw:.2f} degrees")
 
-                    # Map the angles to servo positions (adjust these mappings as needed)
-                    pan_angle = yaw  # Map yaw to pan servo
-                    tilt_angle = roll  # Map pitch to tilt servo
+                # Perform servo control here
+                # For example, to set a servo to a specific angle:
+                servo.setServoPwm('0', roll)
+                servo.setServoPwm('1', yaw)
 
-                    # Limit servo angles to prevent over-rotation (adjust these limits as needed)
-                    pan_angle = max(-90, min(90, pan_angle))
-                    tilt_angle = max(-90, min(90, tilt_angle))
+                time.sleep(poll_interval/1000.0)
 
-                    # Set servo positions using the setServoPulse method from PCA9685
-                    self.servo_pan.setServoPwm(pan_angle)
-                    self.servo_tilt.setServoPulse(tilt_angle)
-
-                    print(f"Pan: {pan_angle:.2f} degrees, Tilt: {tilt_angle:.2f} degrees")
-                    time.sleep(self.poll_interval / 1000.0)
-
-        except KeyboardInterrupt:
-            pass
-
-    def stop(self):
-        # Clean up and stop the servos
-        self.servo_pan.cancel()
-        self.servo_tilt.cancel()
-
-if __name__ == "__main__":
-    gimbal = GimbalControl()
-    gimbal.start()
+    except KeyboardInterrupt:
+        pass
