c71c4648ca46b22b5ecf2cfc994bef452f64d761
Kiriks <kirikakis@gmail.com>
2023-10-12 11:05:17 +0100

    add gitignore and cleanup

--- Code/Server/GimbalControl.py
+++ Code/Server/GimbalControl.py
@@ -20,33 +20,39 @@ else:
 
     # Initialize the Servo class for controlling servos
     servo = Servo()
-    servo_interval = 0.1 #Servo interval in seconds
-    time.sleep(2)
+    servo_interval = 0.4 #Servo interval in seconds
+    time.sleep(1)
 
     poll_interval = imu.IMUGetPollInterval()
     print("Poll Interval: %d ms" % poll_interval)
-    poll_interval = poll_interval/1000
+    poll_interval = 0.2
     time.sleep(1)
 
     try:
-        lastReadTime = time.perf_counter() - poll_interval #Read the first time
-        lastServoTime = time.perf_counter() + servo_interval #Postpone servo the first time
+        lastReadTime = time.perf_counter() #Read the first time
+        lastServoTime = time.perf_counter() #Postpone servo the first time
         while True:
-            if((time.perf_counter() - lastReadTime) > poll_interval):
+            timeElapsed = time.perf_counter() - lastReadTime
+            if(timeElapsed > poll_interval):
                 if imu.IMURead():
+                    print(f"timeElapsed: {timeElapsed}")
+                    print(f"lastReadTime: {lastReadTime}")
+                    print(f"lastServoTime: {lastServoTime}")
                     lastRead = time.time()
                     data = imu.getIMUData()
                     fusionPose = data["fusionPose"]
-                    roll = math.degrees(fusionPose[0]) -90
+                    roll = math.degrees(fusionPose[0])
                     pitch = math.degrees(fusionPose[1])
                     yaw = math.degrees(fusionPose[2])
                     print(f"Roll: {roll:.2f} degrees, Pitch: {pitch:.2f} degrees, Yaw: {yaw:.2f} degrees")
+                    lastReadTime = time.perf_counter()
 
                     if((time.perf_counter() - lastServoTime) > servo_interval):
                         # Perform servo control here
                         # For example, to set a servo to a specific angle:
                         if(roll < 85):
                             roll = 85
+                        print('servo moved')
                         servo.setServoPwm('0', yaw)
                         servo.setServoPwm('1', roll)
                         lastServoTime = time.perf_counter()
