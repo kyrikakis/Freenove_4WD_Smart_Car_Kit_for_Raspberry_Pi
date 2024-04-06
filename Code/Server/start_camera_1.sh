cc56c6b35ba23c5d422b0261824cb82d5bf5ce78
Stefanos Kyrikakis <kirikakis@gmail.com>
2024-04-06 13:31:01 +0100

    add default lens position no autofocus

--- Code/Server/start_camera_1.sh
+++ Code/Server/start_camera_1.sh
@@ -2,5 +2,5 @@
 while :
 do
 	echo 'starting camera 1'
-	rpicam-vid --framerate 30 -n --camera 1 --hdr sensor --width 1366 --height 768 -t 0 --inline --listen -o tcp://0.0.0.0:8888
+	rpicam-vid --framerate 30 -n --lens-position default --camera 1 --hdr sensor --width 1366 --height 768 -t 0 --inline --listen -o tcp://0.0.0.0:8888
 done
